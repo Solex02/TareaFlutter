a1eda07b9a5d8446dfa2446b0ef59ffd38510ecd
Gonzalo <gonzalo_tomas_gmvdj1ma2021@cev.com>
2023-12-18 21:08:22 +0100

    añadido supabase no funciona 😐😞

--- tarea_flutter/lib/pantallaPuntuaciones.dart
+++ tarea_flutter/lib/pantallaPuntuaciones.dart
@@ -1,19 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+
+Future<void> main() async {
+  print("main");
+  await Supabase.initialize(
+    url: 'https://cazcggegzdrzgqqfuete.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhemNnZ2VnemRyemdxcWZ1ZXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5MjAyNDAsImV4cCI6MjAxODQ5NjI0MH0.wXim6D3gmVIPVaBWst-XMlN8jfYrauzqbteXuqFJQ9A',
+  );
+
+  runApp(ScorePage(title: '', score: 0,));
+
+}
+
+// Get a reference your Supabase client
+final supabase = Supabase.instance.client;
 
 class ScorePage extends StatefulWidget {
   const ScorePage({super.key, required this.title, required this.score});
   final String title;
   final int score;
+
   @override
   State<ScorePage> createState() => _ScorePageState();
+  
 }
 
+final products = ["hola", "Hola"];
+
+
 class _ScorePageState extends State<ScorePage> {
-  int get score => score;
   final testMap = {"Gonzalo": 1000, "Pedro": 200};
   final TextEditingController nameController = TextEditingController();
 
+  get score => null;
+
+  @override
+  void initState() {
+    super.initState();
+    getpuntuaciones();
+  }
+
+  // Resto del código...
+
+  void getpuntuaciones() async {
+    print("get");
+    //final response = await supabase.from('puntuaciones').select('*');
+    print('Puntuaciones obtenidas: 2');
+     
+  }
+
+
+
   void guardar() {
+    print("guardar");
     print(nameController.text + score.toString());
   }
 
@@ -38,8 +78,15 @@ class _ScorePageState extends State<ScorePage> {
           body: Column(
             children: [
               Text("Tu puntuacion: $score"),
-              ListView(
-                children: const [Text("Prueba"), Text("Prueba")],
+              SizedBox(
+                height: 200,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: products.length,
+                  itemBuilder: (BuildContext ctxt, int index) {
+                    return new Text("list");
+                  },
+                ),
               ),
               ElevatedButton(
                   onPressed: () {
